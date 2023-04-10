28fc80b6341934514bc9d78211084f57d2119b88
김리나 <77713081+sally132@users.noreply.github.com>
2023-04-10 16:31:21 +0900

    Add files via upload

--- _230411_F.java
+++ _230411_F.java
@@ -6,7 +6,7 @@ public class _230411_F {
 		//바깥 쪽에는 단, 안쪽에는 곱하는 수. (ex. a*b 일때 b)
 		//2단-9단까지 구구단 출력
 		
-		int dan;
+		/*int dan;
 		int su;
 		for(dan = 2; dan<=9; dan++) {//2.
 			System.out.println(dan+"단");
@@ -16,6 +16,18 @@ public class _230411_F {
 			}
 			
 			System.out.println();
+		}*/
+		
+		//교수님
+		int i = 0, j = 0 ;
+		for(i=1; i<10; i++) {
+			for (j=2; j<10; j++) {
+				System.out.print(j+"X"+i+"="+i*j+"\t");
+				//2*1/2*2/2*3
+			}
+			System.out.println();
+			//2*1/3*1/4*1/5*1
+			//1*2/3*1/4*1
 		}
 		
 	}
---
+++ _230411_H.java
@@ -0,0 +1,34 @@
+
+public class _230411_H {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int i =3, j=1;
+			while(j<10) {
+				System.out.println(i+"*"+j+"="+i*j);	
+				j++;
+				
+			}
+			
+			/*교수님 화면에 써져있던 것: int i = 1;
+			 * while (i<6){
+			 * System.out.println(i);
+			 * i++;
+			 * }
+			 * */
+			/*양식:
+			 * int i=0; 
+			 * while (조건){
+			 * 	sum=sum+i;
+			 * 	i++;
+			 * }
+			 */
+			
+			/*양식:
+			 *for(int i=0; i<5; i++){
+			 *	sum=sum+i;
+			 *} 
+			 */
+	}
+
+}
